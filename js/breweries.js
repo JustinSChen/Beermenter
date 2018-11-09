6e6ec0065a1361885a9dd8386a8c5f615787e4be
EtsuN <etnakaha@ucsd.edu>
2018-11-09 14:10:01 +0800

    taken from dev repo

--- js/breweries.js
+++ js/breweries.js
@@ -1,16 +1,25 @@
 $(document).ready(function() {
 	initializePage();
 });
-/*
-function loadNav(){
-    $("#included_nav").load('navigation.html'); 
-}
-*/
+
 function initializePage() {
 	$("#locations").change(function () {
 	    var location = document.getElementById("locations");
 		var loc = "." + location.options[location.selectedIndex].value;
 		$("div.brewery-list").children().show();
-		$("div.brewery-list").children().not(loc).hide();
+		if (loc != ".all") {
+			$("div.brewery-list").children().not(loc).hide();
+		}
 	});
+
+	$(".open-nav").click(function () {
+		console.log("Hi");
+	    $(".sidenav").css("width", "250px");
+	});
+
+	$(".close-nav").click(function () {
+	    $(".sidenav").css("width", "0");
+
+	});
+
 }
